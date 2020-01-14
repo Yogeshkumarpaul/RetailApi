cf68f7ef4ef3421a17653dae205604ee7a43eca4
Yogesh Kumar <developeryogi@gmail.com>
2020-01-14 23:12:12 +0530

    First checkin

---
+++ MiddleLayer/MiddleLayer/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Net.Http.Formatting;
+
+namespace MiddleLayer
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:4200", headers: "*", methods: "*");
+            config.EnableCors(cors);
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MiddleLayer/MiddleLayer/Controllers/TypeDetailsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MiddleLayer.Models;
+
+namespace MiddleLayer.Controllers
+{
+    public class TypeDetailsController : ApiController
+    {
+        private RetailEntities db = new RetailEntities();
+
+        // GET: api/TypeDetails
+        public IQueryable<TypeDetail> GetTypeDetails()
+        {
+            return db.TypeDetails;
+        }
+
+        // GET: api/TypeDetails/5
+        [ResponseType(typeof(TypeDetail))]
+        public IHttpActionResult GetTypeDetail(int id)
+        {
+            TypeDetail typeDetail = db.TypeDetails.Find(id);
+            if (typeDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(typeDetail);
+        }
+
+        // PUT: api/TypeDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTypeDetail(int id, TypeDetail typeDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != typeDetail.TypeId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(typeDetail).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TypeDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TypeDetails
+        [ResponseType(typeof(TypeDetail))]
+        public IHttpActionResult PostTypeDetail(TypeDetail typeDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.TypeDetails.Add(typeDetail);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = typeDetail.TypeId }, typeDetail);
+        }
+
+        // DELETE: api/TypeDetails/5
+        [ResponseType(typeof(TypeDetail))]
+        public IHttpActionResult DeleteTypeDetail(int id)
+        {
+            TypeDetail typeDetail = db.TypeDetails.Find(id);
+            if (typeDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.TypeDetails.Remove(typeDetail);
+            db.SaveChanges();
+
+            return Ok(typeDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TypeDetailExists(int id)
+        {
+            return db.TypeDetails.Count(e => e.TypeId == id) > 0;
+        }
+    }
+}
